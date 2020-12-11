3ba7fe8f6608af2be85350aaa5c7ba95da528705
Marcos de Carvalho Monteiro <mcm@tjrj.jus.br>
2020-12-11 06:43:01 -0300

    Add project files.

---
+++ CaptureAndSave/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿
+namespace CaptureAndSave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textDir = new System.Windows.Forms.TextBox();
+            this.textNumber = new System.Windows.Forms.TextBox();
+            this.textFilename = new System.Windows.Forms.TextBox();
+            this.cboWindows = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(122, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(374, 118);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Alt+PrintScreen+Right";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textDir
+            // 
+            this.textDir.Location = new System.Drawing.Point(122, 102);
+            this.textDir.Name = "textDir";
+            this.textDir.Size = new System.Drawing.Size(425, 20);
+            this.textDir.TabIndex = 4;
+            this.textDir.Text = "c:\\users\\marco\\Downloads";
+            // 
+            // textNumber
+            // 
+            this.textNumber.Location = new System.Drawing.Point(122, 76);
+            this.textNumber.Name = "textNumber";
+            this.textNumber.Size = new System.Drawing.Size(35, 20);
+            this.textNumber.TabIndex = 3;
+            this.textNumber.Text = "1";
+            // 
+            // textFilename
+            // 
+            this.textFilename.Location = new System.Drawing.Point(122, 50);
+            this.textFilename.Name = "textFilename";
+            this.textFilename.Size = new System.Drawing.Size(162, 20);
+            this.textFilename.TabIndex = 2;
+            this.textFilename.Text = "Imagem-";
+            // 
+            // cboWindows
+            // 
+            this.cboWindows.FormattingEnabled = true;
+            this.cboWindows.Location = new System.Drawing.Point(122, 23);
+            this.cboWindows.Name = "cboWindows";
+            this.cboWindows.Size = new System.Drawing.Size(425, 21);
+            this.cboWindows.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(553, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 21);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(122, 252);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 6;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(71, 254);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Repeat:";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(122, 278);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 7;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1500,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 13);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Wait Interval (ms):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Window Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Image name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 79);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Initial Number:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 105);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "Directory Destiny:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(652, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cboWindows);
+            this.Controls.Add(this.textFilename);
+            this.Controls.Add(this.textNumber);
+            this.Controls.Add(this.textDir);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Alt + PrintScreen + Right";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textDir;
+        private System.Windows.Forms.TextBox textNumber;
+        private System.Windows.Forms.TextBox textFilename;
+        private System.Windows.Forms.ComboBox cboWindows;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CaptureAndSave/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaptureAndSave
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        public void SendPrintScreenButtonRight(String WindowName)
+        {
+            IntPtr zero = IntPtr.Zero;
+
+            zero = FindWindow(null, WindowName);
+            if (zero != IntPtr.Zero)
+            {
+                SetForegroundWindow(zero);
+                SendKeys.SendWait("%{PRTSC}{RIGHT}");
+                SendKeys.Flush();
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            GetTaskWindows();
+            textDir.Text = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + "\\Downloads";
+        }
+
+        private void GetTaskWindows()
+        {
+            // Get the desktopwindow handle
+            int nDeshWndHandle = NativeWin32.GetDesktopWindow();
+            // Get the first child window
+            int nChildHandle = NativeWin32.GetWindow(nDeshWndHandle, NativeWin32.GW_CHILD);
+
+            while (nChildHandle != 0)
+            {
+                //If the child window is this (SendKeys) application then ignore it.
+                if (nChildHandle == this.Handle.ToInt32())
+                {
+                    nChildHandle = NativeWin32.GetWindow(nChildHandle, NativeWin32.GW_HWNDNEXT);
+                }
+
+                // Get only visible windows
+                if (NativeWin32.IsWindowVisible(nChildHandle) != 0)
+                {
+                    StringBuilder sbTitle = new StringBuilder(1024);
+                    // Read the Title bar text on the windows to put in combobox
+                    NativeWin32.GetWindowText(nChildHandle, sbTitle, sbTitle.Capacity);
+                    String sWinTitle = sbTitle.ToString();
+                    {
+                        if (sWinTitle.Length > 0)
+                        {
+                            cboWindows.Items.Add(sWinTitle);
+                        }
+                    }
+                }
+                // Look for the next child.
+                nChildHandle = NativeWin32.GetWindow(nChildHandle, NativeWin32.GW_HWNDNEXT);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (cboWindows.Text == "") return;
+            IntPtr zero = IntPtr.Zero;            
+            zero = FindWindow(null, cboWindows.Text);
+            if (zero == IntPtr.Zero) return;
+
+            for (int i = 1; i<= numericUpDown1.Value; i++)
+            {
+                SendPrintScreenButtonRight(cboWindows.Text);
+                if (Clipboard.ContainsImage() == true)
+                {
+                    Clipboard.GetImage().Save(textDir.Text + @"\" + textFilename.Text + textNumber.Text.ToString().PadLeft(4, '0') + @".png", System.Drawing.Imaging.ImageFormat.Png);
+                    textNumber.Text = (int.Parse(textNumber.Text) + 1).ToString();
+                }
+                Thread.Sleep(int.Parse(numericUpDown2.Value.ToString()));
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cboWindows.Items.Clear();
+            GetTaskWindows();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
